9cd6dc97bb8865ad2c11b13f78bcf010f069958f
BruceWouaigne <denis.charrier@knplabs.com>
2012-12-27 13:56:20 +0100

    Save functionality

--- tests/SlapOM/Tests/Units/Connection.php
+++ tests/SlapOM/Tests/Units/Connection.php
@@ -73,6 +73,37 @@ class Connection extends atoum\test
                 ->hasMessage('ERROR Error while filtering dn \'dc=knplabs,dc=com\' with filter \'(&=test)\'.. LDAP ERROR (-7) -- Bad search filter --. Bad search filter');
     }
 
+    public function testModify()
+    {
+        $connection = new \SlapOM\Connection(LDAP_HOST, LDAP_BIND_DN, LDAP_PASSWORD);
+
+        $result = $connection->modify('uid=user.1999,ou=People,dc=knplabs,dc=com', array('mail' => 'newMail@plop.com'));
+
+        $this->assert
+                ->boolean($result)
+                ->isTrue();
+
+        $result = $connection->modify('uid=user.1999,ou=People,dc=knplabs,dc=com', array('mail' => array('newMail1@plop.com', 'newMail2@plop.com', 'newMail3@plop.com')));
+
+        $this->assert
+                ->boolean($result)
+                ->isTrue();
+
+        $this->assert
+                ->exception(function() use ($connection) {
+                            $connection->modify('uid=user.1999,ou=People,dc=knplabs,dc=com', array('objectclass' => 'protectedObjectClass'));
+                        })
+                ->isInstanceOf('\SlapOM\Exception\Ldap')
+                ->hasMessage('ERROR Error while modifying dn \'uid=user.1999,ou=People,dc=knplabs,dc=com\'.. LDAP ERROR (65) -- Object class violation --. Object class violation');
+
+        $this->assert
+                ->exception(function() use ($connection) {
+                            $connection->modify('uid=user.1999,ou=People,dc=knplabs,dc=com', array('l' => null));
+                        })
+                ->isInstanceOf('\SlapOM\Exception\Ldap')
+                ->hasMessage('ERROR Error while modifying dn \'uid=user.1999,ou=People,dc=knplabs,dc=com\'.. LDAP ERROR (21) -- Invalid syntax --. Invalid syntax');                        
+    }
+
 }
 
 class UserForTest1 extends \SlapOM\Entity
