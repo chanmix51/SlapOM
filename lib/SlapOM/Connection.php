8343a767bde2d5b3168b949075e4980196f89854
HUBERT Gregoire (ITG Conseil) <gregoire.hubert.pro@gmail.com>
2013-04-17 10:27:53 +0200

    getMapFor must trim left backslashes only.

--- lib/SlapOM/Connection.php
+++ lib/SlapOM/Connection.php
@@ -50,7 +50,7 @@ class Connection
 
     public function getMapFor($class, $renew = false)
     {
-        $class = trim($class, '\\');
+        $class = ltrim($class, '\\');
         if (!isset($this->maps[$class]) or $renew === true)
         {
             $class_name = sprintf("%sMap", $class);
