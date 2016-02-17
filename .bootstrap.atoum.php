1c62d67a9201627747989a3639afece2770ee213
Sanpi <sanpi@homecomputing.fr>
2016-02-17 14:14:39 +0100

    Use composer autoloader

--- .bootstrap.atoum.php
+++ .bootstrap.atoum.php
@@ -5,10 +5,4 @@ $ini = parse_ini_file(dirname(__FILE__) . '/tests/config/config.ini');
 @define('LDAP_BIND_DN', $ini['bindDn']);
 @define('LDAP_PASSWORD', $ini['password']);
 
-spl_autoload_register(function ($class) {
-    if (0 === strpos($class, 'SlapOM\\'))
-    {
-        $class = str_replace('\\', '/', $class);
-        require sprintf("%s/lib/%s.php", __DIR__, $class);
-    }
-});
+require_once __DIR__ . '/vendor/autoload.php';
